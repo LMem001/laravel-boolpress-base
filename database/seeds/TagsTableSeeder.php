9e9c372870e8bf302f9f8cd0275e75c39857a3b5
Luca <memeoluca6@gmail.com>
2021-05-26 15:30:22 +0200

    added tags seeders

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
 
 class TagsTableSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = [
+            'Informatica',
+            'Elettronica',
+            'Cucina',
+            'Libri',
+            'Piante'
+        ];
+
+        foreach($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+            $newTag->save();
+        }
     }
 }
